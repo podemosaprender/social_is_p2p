47c4a9ab48d3e13a551757484f58e411de78ba37
devel@mauriciocap.com <devel@mauriciocap.com>
2023-11-28 17:13:38 -0500

    OK: added mocha for testing

---
+++ src/social_is_p2p.js
@@ -0,0 +1,7 @@
+//INFO: the full social_is_p2p client in a single plain old js file
+
+function seedIndex_add(link) {
+	return "EMU:1";
+}
+
+console.log("social_is_p2p loaded")
---
+++ src/test.js
@@ -0,0 +1,10 @@
+//SEE: https://mochajs.org/#getting-started
+
+describe('SeedIndex', function () {
+  describe('seedIndex_add', function () {
+		it('should return TODO:EMU', function () {
+			let r= seedIndex_add("TODO:EMU:URL");
+      expect(r).to.equal(-1);
+    });
+  });
+});
